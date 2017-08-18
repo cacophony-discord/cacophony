8d6f6a090cebefe207166f492c160a4afdeb1b3d
Geoffrey ROYER <geoffrey.royer@gmail.com>
2017-08-19 00:51:26 +0200

    Flake8ize urlfilter.py

--- cacophony/hooks/urlfilter.py
+++ cacophony/hooks/urlfilter.py
@@ -2,6 +2,7 @@ import re
 
 URL_REGEX = re.compile(r'https?://')
 
+
 async def urlfilter(app, message):
     """Prevent the bot from sending any answer if there is an URL in it."""
     if URL_REGEX.match(message.content):
