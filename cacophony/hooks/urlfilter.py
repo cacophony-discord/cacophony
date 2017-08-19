bb4b18d1cd92352f8a84b5cb6a65182c5dd89ad5
Geoffrey ROYER <geoffrey.royer@gmail.com>
2017-08-19 18:58:57 +0200

    Fix urlfilter hook

--- cacophony/hooks/urlfilter.py
+++ cacophony/hooks/urlfilter.py
@@ -3,9 +3,9 @@ import re
 URL_REGEX = re.compile(r'https?://')
 
 
-async def urlfilter(app, message):
+async def urlfilter(app, answer):
     """Prevent the bot from sending any answer if there is an URL in it."""
-    if URL_REGEX.match(message.content):
+    if URL_REGEX.match(answer):
         return False
     else:
         return True
