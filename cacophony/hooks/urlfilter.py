08dfe97a4038f52dc2333219d90d4511cd9725d7
Geoffrey ROYER <geoffrey.royer@gmail.com>
2017-08-22 16:22:51 +0200

    Add log in urlfilter

--- cacophony/hooks/urlfilter.py
+++ cacophony/hooks/urlfilter.py
@@ -6,6 +6,7 @@ URL_REGEX = re.compile(r'https?://')
 async def urlfilter(app, answer):
     """Prevent the bot from sending any answer if there is an URL in it."""
     if URL_REGEX.search(answer):
+        app.warning("Detected url. Won't answer.")
         return False
     else:
         return True
