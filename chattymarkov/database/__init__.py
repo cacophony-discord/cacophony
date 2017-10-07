354b8f7a76090f1689334a2527d33ee1e0ae6cd6
Geoffrey ROYER <geoffrey.royer@gmail.com>
2017-10-07 13:31:56 +0200

    Use external chattymarkov package

--- chattymarkov/database/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Chattymarkov database submodule.
-
-This submodule gathers all the supported database formats.
-"""
-
-# flake8: noqa
-from .json import JSONFileDatabase
-from .memory import MemoryDatabase
-from .redis import RedisDatabase
