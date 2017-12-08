0500db2b9943697ab96a2caaf9030c9f8e218176
Geoffrey ROYER <geoffrey.royer@gmail.com>
2017-12-08 14:24:07 +0100

    Update antoinefromafar command

--- cacophony/commands/antoinefromafar.py
+++ cacophony/commands/antoinefromafar.py
@@ -1,16 +1,39 @@
+import re
 import requests
 
+
 from acapela_group.base import AcapelaGroup, AcapelaGroupError
+
+_MENTION_REGEX = re.compile(r'<@([0-9]+)>')
+
+
+def mention_clean(server):
+    members = {str(member.id): member.nick for member in server.members}
+
+    def re_callback(match):
+        id = match.group(1)
+        return members[id]
+
+    return re_callback
 
 
 async def on_antoinefromafar(self, message, *args):
     """Say something with 'AntoineFromAfar' voice on the voice channel."""
-    text_to_say = self.bots[message.server.id].brain.generate()
+    text_to_say = _MENTION_REGEX.sub(
+        mention_clean(message.server),
+        self.bots[message.server.id].brain.generate())
 
+    conf = self.command_config(message.server.id, 'antoinefromafar')
+    username = conf.get('username')
+    password = conf.get('password')
     try:
-        mp3_url = AcapelaGroup().get_mp3_url('sonid15',
-                                             'AntoineFromAfar (emotive voice)',
-                                             text_to_say)
+        acapela = AcapelaGroup()
+        if username is not None and password is not None:
+            self.debug("Will authenticate with %s:%s", username, password)
+            acapela.authenticate(username, password)
+        mp3_url = acapela.get_mp3_url('sonid15',
+                                      'AntoineFromAfar (emotive voice)',
+                                      text_to_say)
     except AcapelaGroupError as err:
         self.warning("Could not get MP3 URL: %s", str(err))
     else:
