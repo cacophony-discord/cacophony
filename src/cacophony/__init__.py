bbe571d8fc69b121144831aa4c9ae5034ec92520
Geoffrey ROYER <geoffrey.royer@gmail.com>
2018-03-08 14:39:39 +0100

    Refactor cacophony package

--- src/cacophony/__init__.py
+++ src/cacophony/__init__.py
@@ -1,1 +1,14 @@
-from .base import *  # noqa
+"""Cacophony Discord Bot."""
+
+__version__ = "0.1.0"
+
+__title = "Cacophony"
+__name__ = "cacophony"
+__description = "Cacophony Discord bot."
+
+__uri__ = "https://github.com/Ge0/cacophony"
+
+__author__ = "Geoffrey ROYER"
+__email__ = "geoffrey.royer@gmail.com"
+
+__copyright__ = "2017, 2018, Geoffrey ROYER"
--- src/cacophony/__main__.py
+++ src/cacophony/__main__.py
@@ -6,11 +6,11 @@ from .app import CacophonyApplication
 
 @click.command()
 @click.argument('name')
-def run(name='cacophony'):
+def main(name='cacophony'):
     """Instanciate an application, then run it."""
     app = CacophonyApplication(name=name)
     app.run()
 
 
 if __name__ == "__main__":
-    run()
+    main()
