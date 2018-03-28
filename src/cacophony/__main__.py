e277efdfa110e459d4df54d12c23355268a14177
Geoffrey ROYER <geoffrey.royer@gmail.com>
2018-03-28 16:38:35 +0200

    Refactor __main__.py

--- src/cacophony/__main__.py
+++ src/cacophony/__main__.py
@@ -4,12 +4,32 @@ import click
 from .app import CacophonyApplication
 
 
+_HANDLERS = {}
+
+
+def handle(action):
+    """Register a function handler for a given action."""
+    def wrap(function):
+        _HANDLERS[action] = function
+        return function
+    return wrap
+
+
 @click.command()
-@click.argument('name')
-def main(name='cacophony'):
+@click.argument('action')
+@click.option('--profile')
+def main(action, profile):
     """Instanciate an application, then run it."""
-    application = CacophonyApplication(name=name)
-    application.run()
+
+    handler = _HANDLERS.get(action)
+    if handler is not None:
+        handler(profile)
+
+
+@handle('run')
+def run(profile, *args, **kwargs):
+    """Run the bot with the specified `profile`."""
+    CacophonyApplication(name=profile).run()
 
 
 if __name__ == "__main__":
