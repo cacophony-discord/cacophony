dd20fdfdd6a3be1ec3e77d3b5d09462246b70dab
Geoffrey ROYER <geoffrey.royer@gmail.com>
2018-04-05 16:36:36 +0200

    Change uri project

--- src/cacophony/__init__.py
+++ src/cacophony/__init__.py
@@ -6,7 +6,7 @@ __title__ = "Cacophony"
 __name__ = "cacophony"
 __description__ = "Cacophony Discord bot."
 
-__uri__ = "https://github.com/Ge0/cacophony"
+__uri__ = "https://github.com/cacophony-discord/cacophony"
 
 __author__ = "Geoffrey ROYER"
 __email__ = "geoffrey.royer@gmail.com"
