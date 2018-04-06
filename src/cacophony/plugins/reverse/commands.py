a807078951212f95ef426735725d77814d52adb4
Geoffrey ROYER <geoffrey.royer@gmail.com>
2018-04-06 14:07:42 +0200

    Remove reverse plugin

--- src/cacophony/plugins/reverse/commands.py
+++
@@ -1,9 +0,0 @@
-"""Plugin commands."""
-
-async def on_reverse(app, message, *args):
-    """Reverse the string in argument and send it back."""
-    if not args:
-        await app.send_message(message.channel,
-                               f"_Usage: {app.prefixize('reverse')} string_")
-    else:
-        await app.send_message(message.channel, ' '.join(args)[::-1])
